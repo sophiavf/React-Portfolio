a1e66621debb51320e5fef8d4487e3676b559c80
sophiavf <15851587+sophiavf@users.noreply.github.com>
2023-06-30 14:27:09 +0200

    Implemented download resume section

--- src/components/DownloadResume.tsx
+++ src/components/DownloadResume.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { content } from "../Content";
+import { Link } from "react-router-dom";
 
 export default function DownloadResume() {
 	const { downloadResume } = content;
 	return (
 		<section className="bg-white">
-			<div className="md:container px-5 pt-14">
+			<div className="md:container px-5 py-14">
 				<h2 className="title" data-aos="fade-down">
 					{downloadResume.title}
 				</h2>
@@ -16,13 +17,15 @@ export default function DownloadResume() {
 				<div className="flex items-center md:flex-row flex-col-reverse ">
 					<div
 						data-aos="fade-left"
-						className="border-2 border-dark_primary max-w-sm
+						className="border-2 border-dark_primary max-w-full
          p-6 shadow-sm rounded-xl rounded-br-[8rem] sm:min-w-[22rem]"
 					>
 						<p className="leading-7">{downloadResume.para}</p>
 						<br />
 						<button className="btn bg-dark_primary text-white">
-							{downloadResume.btnText}
+							<Link to={downloadResume.btnLink} target="_blank">
+								{downloadResume.btnText}
+							</Link>
 						</button>
 					</div>
 				</div>
