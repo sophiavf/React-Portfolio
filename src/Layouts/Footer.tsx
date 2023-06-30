101bf58cb33eca4b36c3e3f91b3753ac9f4d2d17
sophiavf <15851587+sophiavf@users.noreply.github.com>
2023-06-30 13:53:45 +0200

    updated footer to use content from contents file

--- src/Layouts/Footer.tsx
+++ src/Layouts/Footer.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-
+import { content } from "../Content";
 export default function Footer() {
-	const currentYear = new Date().getFullYear();
 	return (
 		<footer className="text-center">
-			<h6 className="mb-3">Sophia Fairbairn</h6>
-			<p>© All CopyRights Reserved{currentYear} </p>
+			<h6 className="mb-3">Sophia </h6> <p>{content.Footer.text}</p>
 		</footer>
 	);
 }
