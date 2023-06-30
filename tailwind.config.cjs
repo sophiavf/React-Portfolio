5fd626ca3f609129a6b3caad067d1af71cf0c1db
sophiavf <15851587+sophiavf@users.noreply.github.com>
2023-06-30 13:46:44 +0200

    updated color scheme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,19 +13,19 @@ module.exports = {
 		},
 		extend: {
 			colors: {
-				dark_primary: "#0D0B0A",
-				bg_light_primary: "#E4B982ff",
+				dark_primary: "#253237",
+				bg_light_primary: "#c2dfe3",
 				gray: "#B7C5D3",
-				bg_medium_primary: "#733F2C",
-				bg_light_secondary: "#D9A05B",
+				bg_medium_primary: "#9db4c0",
+				bg_light_secondary: "#c2dfe3",
 			},
 			backgroundImage: {
-				primaryLinear: "linear-gradient(180deg, #401F14 0.48%, #D9A05B 100%)",
+				primaryLinear: "linear-gradient(180deg, #253237 0.48%, #c2dfe3 100%)",
 			},
 			dropShadow: {
 				primary: "-5px 35px 40px rgba(223, 229, 236, 0.9)",
 			},
-		},
-	},
+				},
+			},
 	plugins: [],
 };
